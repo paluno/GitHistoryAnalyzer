e4b061870f7d86f8a6f03f8ced8ef10a19ddfec3
bb-froggy <ch-git@hannebauer.name>
2015-11-16 14:48:05 +0100

    Renamed to GitHistoryAnalyzer

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using ExpertiseExplorer.Algorithms.RepositoryManagement;
 
-namespace FindGitNewcomers
+namespace GitHistoryAnalyzer
 {
     class Program
     {
